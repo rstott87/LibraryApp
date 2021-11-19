3ea89b05a9102b9fa2f2b61664e0b7e09aca4b32
rstott87 <rstott87@yahoo.com>
2021-11-19 16:46:56 -0500

    added function to delete card

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let myLibrary = []; 
-
+//Main constructor function 
 function Book(title, author, numPages, read) {
     this.title = title
     this.author = author
@@ -9,7 +9,9 @@ function Book(title, author, numPages, read) {
         return `TITLE:  ${title} 
         AUTHOR:  ${author} 
         PAGES:  ${numPages} 
-        READ:  ${read} `;
+        READ:  ${read} 
+        
+        `;
     };
 };
 
@@ -17,22 +19,11 @@ const mainForm = document.querySelector('#mainForm').style;
 
 //selectors and event listeners. functions are defined below this section
 
-/*const submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', addBookToLibrary);*/
-
 const btn = document.getElementById('newBookButton');
 btn.addEventListener('click', addBookToLibrary);
 
 const clearButton =  document.getElementById('clearButton');
 clearButton.addEventListener('click', clearLibrary);
-
-/*const btn2 = document.querySelector('#displayBookButton');
-btn2.addEventListener('click', displayLibrary);*/
-
-//function that pops up new from field
-/*function newBookAdd () {
-mainForm.display = 'block';
-};*/
 
 //function that submits what's in the form then adds it to the new book array
 function addBookToLibrary() {
@@ -42,12 +33,6 @@ function addBookToLibrary() {
     let numPages = prompt ("How many pages was it?");
     let read = prompt ("Did you read it?");
 
-    /*let title = document.getElementById("book").value;
-    let author = document.getElementById("author").value;
-    let numPages = document.getElementById("numPages").value;
-    let read = document.getElementById("read").value;*/
-
-
     console.log (title);
     console.log (author);
     console.log (numPages);
@@ -56,28 +41,46 @@ function addBookToLibrary() {
     var newBook = new Book(title, author, numPages, read);
     myLibrary.push(newBook);
 
+    innerFunction(); 
+
+    };  
+
+
+/*Function that is called in addBookToLibrary to generate the divs that will hold the cards to display. 
+*/     
+function innerFunction() {
     let paragraph = document.getElementById("bookContainer");
     paragraph.innerHTML = ' ';
-    JSON.stringify(myLibrary.forEach((element) => {    
-            let newDiv = document.createElement('div');
-            paragraph.appendChild(newDiv); 
-            newDiv.innerHTML= element.info()
+    JSON.stringify(myLibrary.forEach((element, i) => {
+        let newDiv = document.createElement('div');
+        let innerDiv = document.createElement('div');
+        paragraph.appendChild(newDiv);
+        newDiv.setAttribute('data-card', i);
+        newDiv.innerHTML = element.info();
+        newDiv.appendChild(innerDiv);
+        innerDiv.textContent = 'Delete Entry';
+        innerDiv.setAttribute('class', 'deleteEntry');
+        innerDiv.setAttribute('data-key', i);
+        const deleteEntry = document.querySelectorAll('.deleteEntry');
+        deleteEntry.forEach((button) => {
+            button.addEventListener('click', deleteBook);
             })
-        )}; 
-    
+        })
+    )};
+
 function clearLibrary () {  
     myLibrary = []; 
     let paragraph = document.getElementById("bookContainer");
     paragraph.innerHTML = ' ';
-};
-        
+    };
 
-/* var newBook1 = new Book("How To Change Your Mind", "M. Pollan", 312, "yes");
-var newBook2 = new Book("Mind For Numbers", "Susan", 555, "no");
-
-myLibrary.push(newBook1);
-myLibrary.push(newBook2); */
-
+function deleteBook (e) {
+    const indexPosition = e.target.getAttribute('data-key');
+    console.log (indexPosition);
+    myLibrary.splice(indexPosition, 1);
+    let elementToRemove = document.querySelector(`div[data-card="${indexPosition}"]`);
+    elementToRemove.remove();
+    };
 
 
 
